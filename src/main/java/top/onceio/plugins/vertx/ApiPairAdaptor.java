54f5cf1bf25984363a2080906330045132ee5251
Liar <1520070240@qq.com>
2018-09-09 10:21:01 +0800

    测试跨域问题

--- src/main/java/top/onceio/plugins/vertx/ApiPairAdaptor.java
+++ src/main/java/top/onceio/plugins/vertx/ApiPairAdaptor.java
@@ -334,7 +334,8 @@ public class ApiPairAdaptor {
 			HttpServerRequest req = event.request();
 			req.response().headers().set("Content-Type", "application/json;charset=utf-8");
 			req.response().headers().set("Access-Control-Allow-Origin","*");
-			req.response().headers().set("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,HEAD,OPTIONS,TRACE");
+			//req.response().headers().set("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,HEAD,OPTIONS,TRACE");
+			req.response().headers().set("Access-Control-Allow-Methods","GET,POST,PUT,PATCH");
 			req.response().headers().set("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 			
 			Class<?> returnType = apiPair.getMethod().getReturnType();
